694bd5ab458f5e4fb7cbbd2c1d4b782cabc76e7a
taein Kim <kti0940@gmail.com>
2022-07-12 19:38:27 +0900

    ADD-JWT테스트코드 작성 및 테스트 통과 완료

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,16 @@
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from user.models import User as UserModel
 
 # Create your tests here.
+class LoginUserTest(APITestCase):
+    def setUp(self):
+        
+        self.data = {'username':'youngsang', 'password': '1234'}
+        self.user = UserModel.objects.create_user('youngsang', '1234')
+    
+    def test_login(self):
+        response = self.client.post(reverse('token_obtain_pair'), self.data)
+        self.assertEqual(response.status_code,200)
