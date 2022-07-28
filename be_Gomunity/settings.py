0b9b657e5416574d7ef35482903e7c0034f543e1
Yungsang Hwang <97969957+migdracios@users.noreply.github.com>
2022-07-28 19:44:10 +0900

    Update settings.py

--- be_Gomunity/settings.py
+++ be_Gomunity/settings.py
@@ -38,7 +38,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000',
     'http://localhost:5500',
     'http://localhost:8000',
-    'http://gomunity.shop.s3-website.ap-northeast-2.amazonaws.com/',
+    'http://gomunity.shop.s3-website.ap-northeast-2.amazonaws.com',
 )
 
 CORS_ALLOW_METHODS = (
