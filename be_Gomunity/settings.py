cc4544176fed0b893b08b05134523e4b8ee7afd2
migdracios <migdracios@gmail.com>
2022-07-28 20:35:55 +0900

    Add S3 URL and Access-Control-Allow-Origin

--- be_Gomunity/settings.py
+++ be_Gomunity/settings.py
@@ -38,7 +38,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000',
     'http://localhost:5500',
     'http://localhost:8000',
-    'http://gomunity.shop.s3-website.ap-northeast-2.amazonaws.com/',
+    'http://gomunity.shop.s3-website.ap-northeast-2.amazonaws.com',
 )
 
 CORS_ALLOW_METHODS = (
@@ -60,6 +60,7 @@ CORS_ALLOW_HEADERS = (
     'user-agent',
     'x-csrftoken',
     'x-requested-with',
+    'Access-Control-Allow-Origin',
 )
 
 CSRF_TRUSTED_ORIGINS = (
