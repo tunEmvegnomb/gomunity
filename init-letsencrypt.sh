b692d4551061ac84fb4f2ee78e19bd32f9e0c18f
migdracios <migdracios@gmail.com>
2022-08-02 10:28:52 +0900

    Fix Backend Domain Name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(migdracios.site www.migdracios.site)
+domains=(gomunity.org www.gomunity.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="migdracios@gmail.com" # Adding a valid address is strongly recommended
